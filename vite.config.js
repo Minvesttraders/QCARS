26464a2fd79cc270afd470f1c42ef6f82cfbc103
Minvesttraders <139510173+Minvesttraders@users.noreply.github.com>
2025-06-28 03:57:40 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/minvesttraders.github.io/', // <-- IMPORTANT: Update this if your GitHub Pages URL or repository name changes
-                                    // For a User Page (e.g., username.github.io), base is '/'
-                                    // For a Project Page (e.g., username.github.io/repo-name/), base is '/repo-name/'
-                                    // Given your setup, if minvesttraders.github.io is the repository name itself
-                                    // that serves your app, then this setting is likely correct.
+  base: '/', // <-- Yahan isko '/' karein
 });
-
